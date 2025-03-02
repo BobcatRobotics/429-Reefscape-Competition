6216a9f8af18a5c0f111a2ef9f7390aad05e5635
MollyConnolly <molly.e.connolly@outlook.com>
2025-03-02 14:41:44 -0500

    Added limit switch (commented out)

--- 429-ReefScape/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ 429-ReefScape/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,16 +4,19 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Constants.ArmConstants;
 
 public class ArmSubsystem extends SubsystemBase {
 
   private final TalonFX armMotor;
+  //private final DigitalInput armSwitchPressed;
 
   /** This subsytem that controls the arm. */
   public ArmSubsystem() {
-
+    // Set up a new digital input (for the limit switch)
+    //armSwitchPressed = new DigitalInput(ArmConstants.ARM_SWITCH_PORT);
     // Set up the arm motor as a brushed motor
     armMotor = new TalonFX(ArmConstants.ARM_MOTOR_ID);
 
@@ -26,7 +29,7 @@ public class ArmSubsystem extends SubsystemBase {
     config.CurrentLimits.SupplyCurrentLimitEnable = true;
     config.CurrentLimits.SupplyCurrentLimit = ArmConstants.ARM_MOTOR_CURRENT_LIMIT;
     config.CurrentLimits.StatorCurrentLimitEnable = true;
-    config.CurrentLimits.StatorCurrentLimit = 60;
+    config.CurrentLimits.StatorCurrentLimit = 40;
 
     config.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 0.25; // TODO tune this
     armMotor.getConfigurator().apply(config);
@@ -42,7 +45,12 @@ public class ArmSubsystem extends SubsystemBase {
    * @param speed motor speed from -1.0 to 1, with 0 stopping it
    */
   public void runArm(double speed) {
+    // if the limit switch is pressed, stop the arm motor
+    // if (!armSwitchPressed.get()) {
+    //  armMotor.stopMotor();
+    // } else {
     armMotor.set(speed);
+    // }
   }
 
   public void stopArm() {
