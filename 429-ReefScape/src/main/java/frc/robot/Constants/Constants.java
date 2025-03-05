be5a836cb39604d900af68d768e5ff467574e203
MollyConnolly <molly.e.connolly@outlook.com>
2025-03-05 17:25:16 -0500

    changed arm speed constants

--- 429-ReefScape/src/main/java/frc/robot/Constants/Constants.java
+++ 429-ReefScape/src/main/java/frc/robot/Constants/Constants.java
@@ -30,8 +30,8 @@ public final class Constants {
     public static final int ARM_MOTOR_ID = 9;
     public static final int ARM_MOTOR_CURRENT_LIMIT = 10;
     public static final double ARM_MOTOR_VOLTAGE_COMP = 10;
-    public static final double ARM_SPEED_DOWN = 0.15;
-    public static final double ARM_SPEED_UP = -0.15;
+    public static final double ARM_SPEED_DOWN = 0.177;
+    public static final double ARM_SPEED_UP = -0.19;
     // public static final double ARM_HOLD_DOWN = 0.1;
     // public static final double ARM_HOLD_UP = -0.15;
     public static final int ARM_SWITCH_PORT = 0;
